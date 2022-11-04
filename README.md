aa91df939e8c60997022c8e4356f597c39096acb
khinkyaw1011 <khintheigar1@gmail.com>
2022-11-04 21:54:05 +0630

    first

---
+++ README.md
@@ -0,0 +1,21 @@
+
+## Installation
+
+Install my-project with npm
+
+```bash
+  npm install loaderTest
+  import {showLoader } from 'loaderTest'
+```
+    
+## Read More
+
+Here are some related projects
+
+[MMS IT](https://mms-it.com)
+
+
+## Author
+* U Min Aung
+* MMS It student
+* Weekend Student
---
+++ index.js
@@ -0,0 +1,25 @@
+function showLoader(){
+    const loaderDiv=document.createElement('div');
+    loaderDiv.classList.add('loader')
+    loaderDiv.innerHTML=` <div class="vh-100 d-flex justify-content-center align-items-center fixed-top">
+    <div class=" spinner-border " role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+ </div>`;
+ document.body.append(loaderDiv);
+ }
+ function removeRoader(){
+    document.querySelector('.loader').remove();
+ }
+
+ function showProgress(){
+    const progressDiv=document.createElement('div');
+    progressDiv.classList.add('progress')
+    progressDiv.innerHTML=` <div class="progress fixed-top" style="height:10px">
+    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+  </div>`;
+ document.body.append(progressDiv);
+ }
+ function removeProgress(){
+    document.querySelector('.progress').remove();
+ }
